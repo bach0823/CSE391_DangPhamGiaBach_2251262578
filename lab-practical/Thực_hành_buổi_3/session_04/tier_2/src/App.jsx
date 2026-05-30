e3704a3c6205b7294f21acbb04314cd868f18917
Gia Bách <bachgiadang@gmail.com>
2026-05-30 22:29:10 +0700

    session_04 - tier_2: Hoàn thành bài 2.1

---
+++ lab-practical/Thực_hành_buổi_3/session_04/tier_2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleVariables from "./components/SimpleVariables";
+
+function App() {
+  return (
+    <>
+      <SimpleVariables></SimpleVariables>
+    </>
+  );
+}
+
+export default App;
---
+++ lab-practical/Thực_hành_buổi_3/session_04/tier_2/src/components/SimpleVariables.jsx
@@ -0,0 +1,31 @@
+function SimpleVariables() {
+  // Các biến JavaScript
+  const hientai = new Date().getHours();
+  const ten = "Nguyễn Văn Minh";
+  const tuoi = 20;
+  const quequan = "Hà Nội";
+  const laSinhVien = true;
+  const monHoc = ["HTML", "CSS", "JS", "React"];
+  const canNang = 70;
+  const chieuCao = 1.7;
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>
+        Chào buổi {hientai < 12 ? "sáng" : hientai < 18 ? "chiều" : "tối"} ,
+        {ten}!
+      </h1>
+      <p>Tuổi: {tuoi}</p>
+      <p>Quê quán: {quequan}</p>
+      <p>Sinh viên: {laSinhVien ? "Có" : "Không"}</p>
+      <p>Chiều cao: {chieuCao}m</p>
+      <p>Cân nặng: {canNang}kg</p>
+      <p>BMI: {(canNang / (chieuCao * chieuCao)).toFixed(4)}</p>
+
+      <h2>Môn học yêu thích:</h2>
+      <p>{monHoc.join(", ")}</p>
+    </div>
+  );
+}
+
+export default SimpleVariables;
